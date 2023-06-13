34b674607c1016334579528a223878cf4c61ce36
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-06-13 23:29:57 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,35 +1,28 @@
 
-function createFareMultiplier(number = 5){
-    return function (fare = 60){
-        return fare * number;
-    }
-
-
-}
-
-
-const fareDoubler = createFareMultiplier(2);
-
-const fareTripler = createFareMultiplier(3);
-
-
-const driversArray = ['Antonia', 'Nuru', 'Amari', 'Mo'];
-const returnFirstTwoDrivers = function(driversArray){
-    return [driversArray[0], driversArray[1]]
-}
-
-console.log(returnFirstTwoDrivers(driversArray))
-
-// const driversArray = ['Antonia', 'Nuru', 'Amari', 'Mo'];
-const returnLastTwoDrivers = function (driversArray){
-    return driversArray.slice(-2)
-}
-
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-selectingDrivers[0]();
-
-
-// const driversArray = ['Antonia', 'Nuru', 'Amari', 'Mo'];
-const selectDifferentDrivers =  function (driversArray, returnFirstTwoDrivers){
-    return driversArray(returnFirstTwoDrivers);
-}
+function createFareMultiplier(number = 5) {
+    return function (fare = 60) {
+      return fare * number;
+    };
+  }
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  const driversArray = ["Antonia", "Nuru", "Amari", "Mo"];
+  const returnFirstTwoDrivers = function (driversArray) {
+    console.log(driversArray);
+    return [driversArray[0], driversArray[1]];
+  };
+  
+  // const driversArray = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+  const returnLastTwoDrivers = function (driversArray) {
+    return driversArray.slice(-2);
+  };
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // const driversArray = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+  const selectDifferentDrivers = function (driversArray, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(driversArray);
+  };
